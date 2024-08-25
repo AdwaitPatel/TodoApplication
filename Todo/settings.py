58c71b95f0f75a42eeb2711c63fb0947a6ae5b70
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2024-08-25 10:10:54 +0530

    vercel 3.0

--- Todo/settings.py
+++ Todo/settings.py
@@ -65,12 +65,27 @@ WSGI_APPLICATION = 'Todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'hZtLIgkxINXXwCylYRPTqmYiGSsJsTvR',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
+
+
+
 
 
 # Password validation
